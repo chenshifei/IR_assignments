57eca18b5ac11dad4c2c60c2f5180b3ac09ea00e
shifei.chen <chenshifei.j@gmail.com>
2019-04-16 23:02:50 +0200

    Additional comments in the code

--- lab1/crawler.py
+++ lab1/crawler.py
@@ -7,13 +7,21 @@ r = requests.get('https://www.blocket.se/stockholm/bilar?ca=11&cg=1020')
 soup = BeautifulSoup(r.text, 'html5lib')
 results = []
 
+# Catch the article first instead of calling find_all() all the way
+# so that it's easier to track individual item an assemble them into json
+# objects
 for item in soup.find_all('article', class_='item_row'):
+    # regex to catch the id numbers
     id_str = re.search(r'item_(\d+)', item['id'])
     if id_str:
         car = {}
         car['id'] = id_str.group(1)
+        # Title is an a tag inside a h1 tag
         car['title'] = item.find('h1').find('a', class_='item_link').string
         car['post_time'] = item.find('time')['datetime']
+        # itemprop is a custom tag attribute so BeautifulSoup can't catch its
+        # multiple values
+        # Using regex again to fix it
         location = item.find(itemprop=re.compile(r'availableAtOrFrom'))
         if location:
             car['locaiton'] = location.string
@@ -24,6 +32,7 @@ for item in soup.find_all('article', class_='item_row'):
         if desc:
             car['desc'] = desc
         price = item.find(itemprop='price').string
+        # It's weird but price really can be null
         if price:
             car['price'] = price
         results.append(car)
